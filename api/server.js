63c76f6df980e76263e9f7e0dd5323cab9339672
Rex Zibit <gamemeup8@protonmail.com>
2021-12-21 12:10:23 -0800

    Postman upgrade and environment enhacment

--- api/server.js
+++ api/server.js
@@ -1,20 +1,15 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
-
 const session = require("express-session");
-
 const movieRouter = require("./movie/movie-router.js");
 // const usersRouter = require("./users/users-router.js");
 
 const server = express();
 
-
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
-
-
 
 const sessionConfig = {
   cookie: {
@@ -27,8 +22,7 @@ const sessionConfig = {
   name: "movieapp",
   secret: process.env.COOKIE_SECRET || "keepitsecretflyszonesnapsskeepitsafe",
 };
-// server.use(session(sessionConfig)); 
-
+server.use(session(sessionConfig)); 
 server.use("/api/movie", movieRouter); 
 
 server.get("/", (req, res) => {
