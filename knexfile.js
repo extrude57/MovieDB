03d6b08386ce0d5628794c4825c70220320b9808
Rex Zibit <gamemeup8@protonmail.com>
2021-12-22 16:13:04 -0800

    Working to deploy on main

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
-const pgConnection = process.env.DATABASE_URL || "postgresql://rex:Pass@127.0.0.1:5543/auth";
+const pgConnection = process.env.DATABASE_URL || 
+"postgres://ipqbdwjbuaxnuk:f1c0702d021be3d8f3509872b0bc3ca3cd1e12a7a66b445afd7c6203a232d4a1@ec2-3-228-75-39.compute-1.amazonaws.com:5432/d5nt9r8o8d38go";
 // if using a local postgres server, please create the database manually, Knex will not create it autmatically
 
 module.exports = {
