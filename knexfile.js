62e0f6773eca87667bc782629a7a7887833b728a
Rex Zibit <gamemeup8@protonmail.com>
2021-12-22 18:23:49 -0800

    knexfile update production

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
-
-const pgConnection = process.env.DATABASE_URL || 
-"postgres://ipqbdwjbuaxnuk:f1c0702d021be3d8f3509872b0bc3ca3cd1e12a7a66b445afd7c6203a232d4a1@ec2-3-228-75-39.compute-1.amazonaws.com:5432/d5nt9r8o8d38go";
+const pg  = require("pg");
+const pgConnection = process.env.DATABASE_URL 
 // if using a local postgres server, please create the database manually, Knex will not create it autmatically npm install express-session memorystore
+pg.defaults.ssl = {
+  rejectUnauthorized: false,
+}
 
 module.exports = {
-  testing2: {
+  development: {
     client: "sqlite3",
     useNullAsDefault: true,
     connection: {
@@ -23,7 +25,7 @@ module.exports = {
       directory: "./database/seeds",
     },
   },
-  development: {
+  test2: {
     client: "pg",
     connection: pgConnection,
     ssl: {
