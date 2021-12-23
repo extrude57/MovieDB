93ec55a4608253fd2189a0d17e024adfb0070ba2
Rex Zibit <gamemeup8@protonmail.com>
2021-12-22 17:04:56 -0800

    self signed issue

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const pgConnection = process.env.DATABASE_URL || 
 "postgres://ipqbdwjbuaxnuk:f1c0702d021be3d8f3509872b0bc3ca3cd1e12a7a66b445afd7c6203a232d4a1@ec2-3-228-75-39.compute-1.amazonaws.com:5432/d5nt9r8o8d38go";
-// if using a local postgres server, please create the database manually, Knex will not create it autmatically
+// if using a local postgres server, please create the database manually, Knex will not create it autmatically npm install express-session memorystore
 
 module.exports = {
   testing2: {
